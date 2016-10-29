66564f5f62900a3ea2eb3ae7e1e43a50eaff9af4
ponfarr <chisatofuma@gmail.com>
2016-10-29 23:17:24 +0800

    Update captialize.py

--- captialize.py
+++ captialize.py
@@ -8,8 +8,9 @@ quote = "How can mirrors be real if our eyes aren't real"
 
 def toJadenCase(string):
     list = quote.split()                             #将string分隔开
-    for i in range(len(list)):                       
-        list[i] = list[i][0].upper()+list[i][1:]     #首字母大写
+#     for i in range(len(list)):                       
+#         list[i] = list[i][0].upper()+list[i][1:]    
+    list =[string.capitalize() for string in list]
     str=' '.join(list)                               #将list转回string，并用空格隔开
     return str                                       #返回str
 
@@ -19,5 +20,6 @@ def toJadenCase(string):
 
 2.list->string：
   string = ''.join(list)
-  
+3.咱为何不直接用string的capitalize()呢
+ 
 '''
